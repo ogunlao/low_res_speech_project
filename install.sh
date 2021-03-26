7dd5ad0f1ae9138b267a00e0e1e4557002405808
ogunlao <ogunlao@yahoo.com>
2021-03-26 10:11:27 +0000

    Add bash files

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pip3 install wget
+pip3 install patool
+pip3 install pydub
+# pip3 install torchaudio==0.7.0
+pip3 install soundfile
+# pip install torchtext==0.8.0 torch==1.7.1 pytorch-lightning==1.2.1
+pip3 install nemo-toolkit[all]==1.0.0b1
+sudo apt-get install festival espeak-ng #mbrola
+pip3 install phonemizer
+pip3 install pytorch-lightning==0.9.0
+pip3 install torch==1.7.1
+pip3 install torchtext==0.8.0
--- utils.py
+++ utils.py
@@ -15,17 +15,17 @@ def collate_args(args1, args2):
     return args
       
 def calc_distr(df, punct_set):
-  chars = defaultdict(int)
-  for sentence in df.sentence.values:
-    sentence = normalize('NFD', sentence).encode('ascii', 'ignore')
-    sentence = sentence.decode('UTF-8')
-    for char in sentence:
-      if char not in punct_set:
-        if char == " ":
-          chars["<sep>"]+=1
-        else: chars[char.lower()]+=1
-  
-  #chars_sorted = sorted(chars.items())
-  #print(chars_sorted)
-  
-  return chars
+    chars = defaultdict(int)
+    for sentence in df.sentence.values:
+        sentence = normalize('NFD', sentence).encode('ascii', 'ignore')
+        sentence = sentence.decode('UTF-8')
+        for char in sentence:
+            if char not in punct_set:
+                if char == " ":
+                    chars["<sep>"]+=1
+                else: chars[char.lower()]+=1
+
+    #chars_sorted = sorted(chars.items())
+    #print(chars_sorted)
+
+    return chars
